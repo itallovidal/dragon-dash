abebe2fa6c6327195a58f75e6c06918ffb873e7a
itallovidal <itallovidalll@gmail.com>
2025-04-10 18:35:59 -0300

    add specific behavior to eletric magic power

--- Assets/scripts/Power_up/MagicScript.cs
+++ Assets/scripts/Power_up/MagicScript.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 public class MagicScript : MonoBehaviour
 {
     private string[] powerupMagics = { "ice_magic", "eletric_magic", "fire_magic" };
+    private string magicTag;
+
+    void Start()
+    {
+        magicTag = gameObject.tag;
+    }
     void Update()
     {
-        transform.position += Vector3.right * Time.deltaTime * 4f;
+        Debug.Log("tag");
+        Debug.Log(magicTag);
+        if (magicTag == "eletric_magic")
+        {
+            float verticalOffset = Random.Range(-4f,4f);
+            Vector3 direction = new Vector3(1f, verticalOffset, 0f);
+            transform.position += direction * Time.deltaTime * 4f;
+        }
+        else
+        {
+            transform.position += Vector3.right * Time.deltaTime * 4f;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
