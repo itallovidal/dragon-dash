bbe4c71b0527e9b25a8dd5e74c547ee4f5365a66
itallovidal <itallovidalll@gmail.com>
2025-04-10 18:20:26 -0300

    add physics to iceball

--- Assets/scripts/Power_up/MagicScript.cs
+++ Assets/scripts/Power_up/MagicScript.cs
@@ -1,7 +1,9 @@
+using System.Linq;
 using UnityEngine;
 
 public class MagicScript : MonoBehaviour
 {
+    private string[] powerupMagics = { "ice_magic", "eletric_magic", "fire_magic" };
     void Update()
     {
         transform.position += Vector3.right * Time.deltaTime * 4f;
@@ -9,6 +11,9 @@ public class MagicScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        if (!powerupMagics.Contains(collision.gameObject.tag))
+        {
+            Destroy(gameObject);
+        }
     }
 }
