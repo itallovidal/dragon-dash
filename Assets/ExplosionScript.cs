c0652e3e4d0713d2198c5e771e425e9068a14b66
¨Gabriel <devgabrielnathan@gmail.com>
2025-04-29 15:05:06 -0300

    adding animation destruction logic

--- Assets/ExplosionScript.cs
+++ Assets/ExplosionScript.cs
@@ -2,20 +2,15 @@ using UnityEngine;
 
 public class ExplosionScript : MonoBehaviour
 {
-    private void Start()
+    public Animator animator;
+
+    private void Update()
     {
-        // Obtém o tempo de duração da animação e destrói o objeto após esse tempo
         Animator animator = GetComponent<Animator>();
         if (animator != null)
         {
-            // Calcula a duração da animação com base no tempo total do clipe
             float animationDuration = animator.GetCurrentAnimatorStateInfo(0).length;
             Destroy(gameObject, animationDuration);
-        }
-        else
-        {
-            // Caso não tenha um Animator, destrói após um tempo padrão
-            Destroy(gameObject, 1f);
         }
     }
 }
