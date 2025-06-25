555e129ce554b59970f798003f54e5de4b189a7e
Leonardo P <leopatrosilva@gmail.com>
2025-06-25 12:59:39 -0300

    add highscore for activeScene

--- Assets/scripts/Managers/GameLogic.cs
+++ Assets/scripts/Managers/GameLogic.cs
@@ -18,6 +18,8 @@ public class GameLogic : MonoBehaviour
     private PlayerScript playerScript;
     private AudioManagerScript audioManager;
 
+    private string activeScene;
+
     private void Awake()
     {
         // Se não encontrar, busca na cena
@@ -25,9 +27,9 @@ public class GameLogic : MonoBehaviour
         {
             audioManager = FindFirstObjectByType<AudioManagerScript>();
         }
-        
+
         // Se não tiver enccontrado sinaliza
-        if (audioManager == null)   
+        if (audioManager == null)
         {
             Debug.LogWarning("AudioManager não encontrado na cena!");
         }
@@ -35,9 +37,29 @@ public class GameLogic : MonoBehaviour
  
     void Start()
     {
-        highScore = PlayerPrefs.GetInt("highscore", 0);
-        highScoreText.gameObject.GetComponent<TextMeshProUGUI>().text = "HIGHSCORE: " + highScore.ToString();
+        activeScene = SceneManager.GetActiveScene().name;
+        StartHighScore();
         playerScript = FindFirstObjectByType<PlayerScript>();
+    }
+
+    private void StartHighScore()
+    {
+        switch (activeScene)
+        {
+            case "clouds_level":
+                highScore = PlayerPrefs.GetInt("Clouds_highscore", 0);
+                break;
+            case "florest_level":
+                highScore = PlayerPrefs.GetInt("Florest_highscore", 0);
+                break;
+            case "space_level":
+                highScore = PlayerPrefs.GetInt("Space_highscore", 0);
+                break;
+            default:
+                Debug.Log("Cena não reconhecida para o highscore: " + activeScene);
+                break;
+        }
+        highScoreText.gameObject.GetComponent<TextMeshProUGUI>().text = "HIGHSCORE: " + highScore.ToString();
     }
 
     [ContextMenu("Add 1 point to score")]
@@ -47,10 +69,28 @@ public class GameLogic : MonoBehaviour
         scoreText.gameObject.GetComponent<TextMeshProUGUI>().text = playerScore.ToString();
 
         if (highScore < playerScore) {
-            PlayerPrefs.SetInt("highscore", playerScore);
+            UpdateHighScore();
         }
     }
 
+    private void UpdateHighScore()
+    {
+        switch (activeScene)
+        {
+            case "clouds_level":
+                PlayerPrefs.SetInt("Clouds_highscore", playerScore);
+                break;
+            case "florest_level":
+                PlayerPrefs.SetInt("Florest_highscore", playerScore);
+                break;
+            case "space_level":
+                PlayerPrefs.SetInt("Space_highscore", playerScore);
+                break;
+            default:
+                Debug.LogWarning("Cena não reconhecida para atualizar o highscore: " + activeScene);
+                break;
+        }
+    }
     public void RestartGame()
     {
         string text = overlayText.gameObject.GetComponent<TextMeshProUGUI>().text;
@@ -67,7 +107,7 @@ public class GameLogic : MonoBehaviour
         }
 
         Time.timeScale = 1f;
-        
+
         if (audioManager != null)
         {
             audioManager.audioSource.volume = 0.2f;
